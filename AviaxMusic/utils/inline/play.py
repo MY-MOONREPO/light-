6b0c9a94c32942c4b047464ca903f4c57ac7b6df
MOONl <mypersonalheruko@gmail.com>
2025-01-22 07:14:26 +0530

    Update play.py

--- AviaxMusic/utils/inline/play.py
+++ AviaxMusic/utils/inline/play.py
@@ -56,16 +56,16 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            #InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url=f"https://t.me/untold_coder4"
+                text="𝐔ᴘᴅᴀᴛᴇs", url=f"https://t.me/MoonMusic_Updates"
             ),
             InlineKeyboardButton(
-                text="Bᴏᴛ", url=f"https://t.me/untold_coder"
+                text="𝗖ʟᴏɴᴇ", url=f"https://t.me/MoonMusic_Updates/687"
             ),
         ],
     ]
@@ -77,16 +77,16 @@ def stream_markup(_, chat_id):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            #InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url=f"https://t.me/untold_coder4"
+                text="𝐔ᴘᴅᴀᴛᴇs", url=f"https://t.me/MoonMusic_Updates"
             ),
             InlineKeyboardButton(
-                text="Bᴏᴛ", url=f"https://t.me/untold_coder"
+                text="𝗖ʟᴏɴᴇ", url="https://t.me/MoonMusic_Updates/687"
             ),
         ],
     ]
